db5ebf70f0ca0ee8e43e0cb667df9e3482ec4bc9
zero <doing4zero@gmail.com>
2023-02-09 21:47:44 +0700

    Initialize

---
+++ app/screens/HomeScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+export default function App() {
+  return <SafeAreaView></SafeAreaView>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
